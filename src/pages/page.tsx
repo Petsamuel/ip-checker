0b39301ef48f45900159bf10e4ea04c48a7c962c
Samuel Peters <65086865+Petsamuel@users.noreply.github.com>
2024-02-21 09:02:10 +0100

    Update page.tsx

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,27 +1,24 @@
-export default function Testing() {
-  // export const nav = [
-  //   {
-  //     name: "home",
-  //     link: "",
-  //   },
-  //   {
-  //     name: "home",
-  //     link: "",
-  //   },
-  // ];
+export const Testing = ()=>{
+   export const nav = [
+     {
+       name: "home",
+       link: "",
+    },
+    {
+      name: "about",
+     link: "",
+    },
+   ];
   return (
     <div>
       <header className="fixed w-screen text-black bg-white shadow-md py-4">
         <nav className=" relative flex content-around justify-around items-center">
           <div className="brand">Brand Logo</div>
-          <ul className="flex gap-4">
-            <li className="uppercase">home</li>
-            <li className="uppercase">about</li>
-            <li className="uppercase">team</li>
-            <li className="uppercase">gallery</li>
-            <li className="uppercase">event</li>
-            <li className="uppercase">support moyi</li>
-            <li className="uppercase">contact</li>
+          <ul className="flex gap-4 lg:flex-row flex-col">
+
+              {nav.map((val, key)=>(
+            <li className="uppercase" kay={val.key}>{val.name}</li>))}
+           
           </ul>
         </nav>
       </header>
